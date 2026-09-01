25d08b97330875696a24d48b130e3fc2af97a2ef
skzidev <chervonijpes@gmail.com>
2026-08-31 22:58:23 -0400

    chore: setup CI/CD

--- mock/user_code.zig
+++ mock/user_code.zig
@@ -20,8 +20,6 @@ pub fn main(init: velox.Init) !void {
     const motor = try velox.Motor.init(5, .blue, .forward, .coast);
     motor.spinAt(100, .rpm);
 
-    _ = try init.io.concurrent(monitorThread, .{init.io});
-
     while (true) {
         try init.io.sleep(
             std.Io.Duration.fromMilliseconds(2),
