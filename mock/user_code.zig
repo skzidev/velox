04527bb2e8d4c653b886c2eeff03ea390cc6fe4a
skzidev <chervonijpes@gmail.com>
2026-09-15 00:47:37 -0400

    simplify user code

--- mock/user_code.zig
+++ mock/user_code.zig
@@ -9,6 +9,4 @@ fn teleop() void {}
 /// Example user program for the Velox SDK.
 pub fn main(init: velox.Init) !void {
     try stdout.writeStreamingAll(init.io, "booting up...\n");
-    const motor = try velox.Motor.init(1, .blue, .forward, .coast);
-    motor.spinAt(100, .rpm);
 }
